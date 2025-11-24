72fbae7ce4d8e16cbacb25714f8202440891f91f
Volodymyr <volodymyr.tsukanov.23@gmail.com>
2025-11-24 10:00:21 +0100

    lab8 setup

---
+++ src/main/java/lab/Spring/Ate.java
@@ -0,0 +1,12 @@
+package lab.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** not Eight */
+@SpringBootApplication
+public class Ate {
+    public static void main(String[] args) {
+        SpringApplication.run(Ate.class, args);
+    }
+}
