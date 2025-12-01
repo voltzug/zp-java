f0dbdf893c337c9b81a75bce5c9cf6c01ec3f19c
Volodymyr <volodymyr.tsukanov.23@gmail.com>
2025-12-01 10:00:20 +0100

    lab9 fix spring

--- src/main/java/lab/Spring/med/repository/PatientRepository.java
+++
@@ -1,9 +0,0 @@
-package lab.Spring.med.repository;
-
-import lab.Spring.med.model.Patient;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PatientRepository extends JpaRepository<Patient, Long> {
-}
